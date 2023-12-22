218da63567392426adb00fb7971f1d3ef0743fc1
volland <volland@hotmail.com>
2023-12-22 21:53:15 +0300

    2023/09/30 00:00|Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле" Самостоятельная работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле" 1. Создайте новый проект 2. Запустите новый проект в Pycharm
    Ваша задача:
    Создайте список из 5 машин - ["BMW", "MB", "LADA", "KIA", "HONDA"]
    Создайте цикл for и в цикле распечатайте каждый из автомобилей в строке 'Я езжу на автомабиле марки'
    Создайте целочисленную переменную cars_count = 0
    Напишите в цикле из шага номер 2 увеличение переменной на 10 в каждом шаге цикла (cars_count += 10)
    Посмотрите на консоль и на ход выполнения программы

--- probe.py
+++ probe.py
@@ -1,5 +1,5 @@
-my_cars=['BMW','MB','LADA','KIA','HONDA']
+my_cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
 cars_count = 0
 for car in my_cars:
     cars_count += 10
-    print('Я езжу на автомабиле марки '+car)
+    print('Я езжу на автомабиле марки ' + car)
