189fda14e864903db85de36f2fcd8c4df797b27e
volland <volland@hotmail.com>
2023-12-23 15:39:17 +0300

    2023/10/05 00:00|Домашняя работа по уроку "Пространство имен и способы вызова функции"

---
+++ main.py
@@ -0,0 +1,18 @@
+def test():
+    a, b = 'my', 'homework'
+    print(a)
+    print(b)
+
+
+test()
+
+print(30 * '-')
+
+
+def test2(param1, param2, param3):
+    print(param1)
+    print(param2)
+    print(param3)
+
+
+test2('my homework', 'is', 'my current task')
