59b5251adfe2c960489334a961c18bbff783a6fa
volland <volland@hotmail.com>
2023-12-26 12:55:30 +0300

    remormated

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ def test(*args):
         print(arg)
 
 
-test('С наступающим новым',2024,'годом',{'int': 3, 'str': 'cnh 4', 'bool': True}, [3, 'cnh 4', True])
+test('С наступающим новым', 2024, 'годом', {'int': 3, 'str': 'cnh 4', 'bool': True}, [3, 'cnh 4', True])
+
 
 def factorial(n):
-    if n!=1:
-        factorial_n_minus_1=factorial(n=n-1)
-        return factorial_n_minus_1*n
+    if n != 1:
+        factorial_n_minus_1 = factorial(n=n - 1)
+        return factorial_n_minus_1 * n
     else:
         return 1
-user_value=input('Введите значение для вычисления факториала:')
+
+
+user_value = input('Введите значение для вычисления факториала:')
 test(f'{user_value}!={factorial(int(user_value))}')
