6920123cd98ac3dca9c98a1079e446c4fbdfd9db
volland <volland@hotmail.com>
2024-01-19 23:30:09 +0300

    2023/10/30 00:00|Практика. Часть 1. Создание человека людей в списке Оптимизирован Copilot

--- difference_class_instance_attributes.py
+++ difference_class_instance_attributes.py
@@ -2,11 +2,19 @@ import random
 
 from termcolor import cprint
 
+# Константы
+MIN_FULLNESS = 20
+MAX_FULLNESS = 100
+FOOD_COST = 50
+WORK_INCOME = 50
+INIT_FOOD = 10
+INIT_MONEY = 50
+DAYS = 365
 
 class Man:
     def __init__(self, name):
         self.name = name
-        self.fullness = 50
+        self.fullness = MAX_FULLNESS // 2
         self.house = None
 
     def __str__(self):
@@ -15,77 +23,62 @@ class Man:
     def eat(self):
         if self.house.food >= 10:
             cprint(f'{self.name} поел', color='yellow', force_color=True)
-            self.fullness += 10
+            self.fullness = min(self.fullness + 10, MAX_FULLNESS)
             self.house.food -= 10
         else:
             cprint(f'{self.name} нет еды', color='light_red', force_color=True)
 
     def shopping(self):
-        if self.house.money >= 50:
+        if self.house.money >= FOOD_COST:
             cprint(f'{self.name} сходил в магаззин за едой', color='magenta', force_color=True)
-            self.house.money -= 50
-            self.house.food += 50
+            self.house.money -= FOOD_COST
+            self.house.food += FOOD_COST
         else:
             cprint(f'{self.name} деньги закончились', color='red', force_color=True)
 
     def work(self):
         cprint(f'{self.name} сходил на работу', color='blue', force_color=True)
-        self.house.money += 50
-        self.fullness -= 10
+        self.house.money += WORK_INCOME
+        self.fullness = max(self.fullness - 10, 0)
 
     def watch_MTV(self):
         cprint(f'{self.name} смотрел MTV целый день', color='green', force_color=True)
-        self.fullness -= 10
+        self.fullness = max(self.fullness - 10, 0)
 
     def go_into_house(self, house):
         self.house = house
         cprint(f'{self.name} въехал в дом!!!', color='cyan', force_color=True)
 
     def act(self):
-        if self.fullness < 0:
+        if self.fullness <= 0:
             cprint(f'{self.name} умер...', color='light_red', force_color=True)
             return
-        dice = random.randint(1, 6)
-        if self.fullness < 20:
+        if self.fullness < MIN_FULLNESS:
             self.eat()
         elif self.house.food < 10:
             self.shopping()
-        elif self.house.money < 50:
+        elif self.house.money < FOOD_COST:
             self.work()
-        elif dice == 1:
-            self.work()
-        elif dice == 2:
-            self.eat()
         else:
-            self.watch_MTV()
-        # elif dice == 3:
-        #     self.eat()
-        # elif dice == 4:
-        #     self.eat()
-        # elif dice == 5:
-        #     self.eat()
+            random.choice([self.work, self.eat, self.watch_MTV])()
 
 
 class House:
     def __init__(self):
-        self.food = 10
-        self.money = 50
+        self.food = INIT_FOOD
+        self.money = INIT_MONEY
 
     def __str__(self):
         return f'в доме осталось еды {self.food}, денег {self.money}'
 
 
-citizens = [
-    Man(name='Бивис'),
-    Man(name='Батхед'),
-    Man(name='Кенни'),
-]
+citizens = [Man(name) for name in ('Бивис', 'Батхед', 'Кенни')]
 
 my_sweet_home = House()
 for citizen in citizens:
     citizen.go_into_house(house=my_sweet_home)
 
-for day in range(1, 366):
+for day in range(1, DAYS + 1):
     cprint(f'==================== День {day} ====================')
     for citizen in citizens:
         citizen.act()
@@ -93,11 +86,3 @@ for day in range(1, 366):
     for citizen in citizens:
         print(citizen)
     print(my_sweet_home)
-
-# print(vasya)
-# vasya.eat()
-# print(vasya)
-# vasya.work()
-# print(vasya)
-# vasya.play_DOTA()
-# print(vasya)
